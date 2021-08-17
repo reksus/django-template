f5b9b09941fdee889ac4dc74a5ff09abb8dd1a31
Tarun Tyagi <mail2ttarun@gmail.com>
2021-08-17 15:35:40 +0530

    added docstring as applicable

--- sample_app/views.py
+++ sample_app/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render
+"""samples view to test the functionalities of external services added to the project."""
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .tasks import sample_heavy_task
 
+
 # Create your views here.
 class SampleAPIView(APIView):
+	"""sample class to test the working project settings."""
+
 	def get(self, request):
+		"""Sample get request which return a sample json response."""
 		response_data = {
-			'status' : "success",
-			'sCode' : 200,
+			'status': "success",
+			'sCode': 200,
 			'data': "sample data",
-			'message' : "sample data successfully retrieved"
+			'message': "sample data successfully retrieved"
 		}
 		return Response(data=response_data, status=status.HTTP_200_OK)
 
 
 class SampleBgTaskAPIView(APIView):
-	def get(self, request):
+	"""sample class to test task queue project settings."""
 
+	def get(self, request):
+		"""Sample get request which enqueues a sample background task."""
 		# shift this heavy task to a task queue and continue with the request/response cycle
 		sample_heavy_task.send()
 
